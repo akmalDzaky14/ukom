2eb750f0c54724ba6ac5ea576e93ecdfbf0138a8
Akmal Dzaky Hafidhuddin <50478173+akmalDzaky14@users.noreply.github.com>
2023-02-24 15:49:11 +0700

    Update db.php

--- db.php
+++ db.php
@@ -6,7 +6,7 @@
     public static function getInstance() {
       if (!isset(self::$instance)) {
        
-        self::$instance = mysqli_connect("localhost", "root", "root", "assignment2");
+        self::$instance = mysqli_connect("localhost", "root", "root", "ukom");
       }
       return self::$instance;
     }
